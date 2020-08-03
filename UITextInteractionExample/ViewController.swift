2d565372bac44d2d7c48a36cbf8a3d70e29176db
Mark Vitale <mavitale@microsoft.com>
2020-08-03 14:59:42 -0700

    Add more complex test cases to the test app

--- UITextInteractionExample/ViewController.swift
+++ UITextInteractionExample/ViewController.swift
@@ -3,31 +3,78 @@
 import UIKit
 
 class ViewController: UIViewController {
-	private static let initialText = """
-	Sphinx of black quartz, judge my vow.
-	Pack my box with five dozen liquor jugs.
-	"""
+	/// Our text label
+	let customTextLabel = CustomTextLabel(labelText: ViewController.initialText)
+	
+	/// A switch for toggling between
+	let interactionModeSwitch = UISwitch()
+	
+	var interaction: UITextInteraction?
 
 	override func loadView() {
 		view = UIView(frame: .zero)
 		
-		// Create a custom text label and add it as a subview
-		let customTextLabel = CustomTextLabel(labelText: ViewController.initialText)
+		// Make a button to rotate the text
+		let rotateButton = UIButton(type: .roundedRect)
+		rotateButton.setTitle("Rotate", for: .normal)
+		rotateButton.addTarget(self, action: #selector(rotateTextLabel), for: .touchUpInside)
+		
+		// Make a switch to toggle interaction mode
+		let interactionModeLabel = UILabel()
+		interactionModeLabel.text = "Editable"
+		interactionModeSwitch.addTarget(self, action: #selector(updateInteractionMode), for: .valueChanged)
+		interactionModeSwitch.setOn(true, animated: false)
+		let interactionModeStackView = UIStackView(arrangedSubviews: [interactionModeLabel, interactionModeSwitch])
+		interactionModeStackView.spacing = 10.0
+		
+		// Put our modifier controls into a stack view
+		let labelModifierControls = UIStackView(arrangedSubviews: [
+			rotateButton,
+			interactionModeStackView
+		])
+		labelModifierControls.translatesAutoresizingMaskIntoConstraints = false
+		labelModifierControls.axis = .vertical
+		labelModifierControls.alignment = .leading
+		view.addSubview(labelModifierControls)
+		
+		// Add the CustomTextLabel as a subview
 		customTextLabel.translatesAutoresizingMaskIntoConstraints = false
 		view.addSubview(customTextLabel)
 		
+		
 		NSLayoutConstraint.activate([
-			customTextLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
-			customTextLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+			labelModifierControls.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+			labelModifierControls.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+			customTextLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 200),
+			customTextLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 200),
 		])
 		
-		// Add UITextInteraction based on the customTextLabel
-		let interaction = UITextInteraction(for: .editable)
-		interaction.textInput = customTextLabel
-		view.addInteraction(interaction)
-		
+		updateInteractionMode()
 	}
-
-
+	
+	/// Rotate the text label by 45 degrees
+	@objc func rotateTextLabel() {
+		// Transform the label by rotating pi/4 radians
+		customTextLabel.transform = customTextLabel.transform.concatenating(CGAffineTransform(rotationAngle: .pi / 8))
+	}
+	
+	/// Toggle whether we should be using a
+	@objc func updateInteractionMode() {
+		if let oldInteraction = interaction {
+			view.removeInteraction(oldInteraction)
+		}
+		
+		// Add UITextInteraction based on the customTextLabel
+		let newInteraction = UITextInteraction(for: interactionModeSwitch.isOn ? .editable : .nonEditable)
+		newInteraction.textInput = customTextLabel
+		view.addInteraction(newInteraction)
+		interaction = newInteraction
+	}
+	
+	/// The initial text to populate our `CustomTextLabel`
+	private static let initialText = """
+	Sphinx of black quartz, judge my vow.
+	Pack my box with five dozen liquor jugs.
+	"""
 }
 
